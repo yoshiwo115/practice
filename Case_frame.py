227b0bcbffb1f9c9d7789397759bc4b7405b5ea8
yoshiwo115 <yoshiwo115@gmail.com>
2021-09-22 16:37:54 +0900

    メモリ開放しながらxmlを読むコード

--- Case_frame.py
+++ Case_frame.py
@@ -7,10 +7,23 @@ declinable_word = "綺麗"
 
 import xml.etree.ElementTree as ET
 
-#xmlデータを読み込み
-tree = ET.parse('kyoto-univ-web-cf-2.0.xml')
-#一番上の階層の要素取り出し
-root = tree.getroot()
+# xmlデータを一気に読み込む場合
+# 重すぎてできなかった
+# tree = ET.parse('kyoto-univ-web-cf-2.0.xml')
+# 一番上の階層の要素取り出し
+# root = tree.getroot()
+# print(root.tag)
+# print(root.attrib)
 
-print(root.tag)
-print(root.attrib)
+
+# メモリ開放しながら読む
+
+context = ET.iterparse('kyoto-univ-web-cf-2.0.xml', events=('start', 'end'))
+
+_, entry = next(context)  # 一つ進めて root を得る
+
+for event, elem in context:
+    if event == 'end' and elem.tag == 'component':
+        # do something on component
+        print(elem.text)
+        entry.clear()  # 用が済んだらrootを空に
