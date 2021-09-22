ba98640911026881a03488ee94063918fe946395
yoshiwo115 <yoshiwo115@gmail.com>
2021-09-22 14:04:29 +0900

    コメント付け足し

--- input.py
+++ input.py
@@ -1,26 +1,29 @@
-#Ginza準備
+# search_word決定機構
+
+# Ginza準備
 import spacy
 from spacy import displacy
 nlp = spacy.load('ja_ginza')
-#ここまで
 
-#形態素解析
-
-#ユーザー入力
+# ユーザー入力
 input_dialogue = input()
-#Ginzaに通す
+
+# 形態素解析------------------------------------------------------------
+# Ginzaに通す
 doc = nlp(input_dialogue)
 
-#名詞保存変数グローバル宣言
+# 名詞保存変数グローバル宣言
 noun_word = ""
-#用言保存変数グローバル宣言
+# 用言保存変数グローバル宣言
 declinable_word = ""
 
 for sent in doc.sents:
     for token in sent:
         if token.pos_=="NOUN":
-            noun_word = token.orth_ #名詞抜き出し
+            # 名詞抜き出し
+            noun_word = token.orth_ 
         elif token.pos_=="ADJ" or token.pos_=="VERB":
-            declinable_word = token.orth_ #用言抜き出し
+            # 用言抜き出し
+            declinable_word = token.orth_
 
 print("search_word:", noun_word, declinable_word)
