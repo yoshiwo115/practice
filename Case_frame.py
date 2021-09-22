ec44b7e783daa27a7ccd1b6441254e91903b606d
yoshiwo115 <yoshiwo115@gmail.com>
2021-09-22 16:16:32 +0900

    読み込みfileパス変更

--- Case_frame.py
+++ Case_frame.py
@@ -8,7 +8,7 @@ declinable_word = "綺麗"
 import xml.etree.ElementTree as ET
 
 #xmlデータを読み込み
-tree = ET.parse('../../kyoto-univ-web-cf-2.0/kyoto-univ-web-cf-2.0.xml')
+tree = ET.parse('kyoto-univ-web-cf-2.0.xml')
 #一番上の階層の要素取り出し
 root = tree.getroot()
 
