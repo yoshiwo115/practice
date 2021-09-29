988d65b9b3672136342bf692547edef73271ec6d
yoshiwo115 <yoshiwo115@gmail.com>
2021-09-29 17:35:48 +0900

    コメント追加

--- Case_frame.py
+++ Case_frame.py
@@ -9,18 +9,28 @@ declinable_word = "綺麗"
 
 import xml.etree.ElementTree as ET
 
-# メモリ開放しながら読みたい
+# メモリ開放しながらxmlを読む
 
 context = ET.iterparse('kyoto-univ-web-cf-2.0.xml', events=('start', 'end'))
 
 for event,elem in context:
-    if event == 'start' and elem.tag == 'entry' and 'あえない' in elem.attrib['headword']:
-            print(elem.attrib)
+    if event == 'start' and elem.tag == 'entry' and 'あえない' in elem.attrib['headword']: 
+        # headwordが特定の用言だったとき
+            
+            print(elem.attrib) #何で検索されているか表示
+            
+            for event, elem in context: 
+                #entry以下でループを回す
+                
+                if event == 'end' and elem.tag == 'entry': 
+                    #entryのendタグが来たら
+                    
+                    break #ループを抜ける
 
-            for event, elem in context:
-                if event == 'end' and elem.tag == 'entry':
-                    break
-                elif event == 'end' and elem.tag == 'component':
-                    print(elem.text)
+                elif event == 'end' and elem.tag == 'component': 
+                    #componentのendタグか来たら
+                    
+                    print(elem.text) #内容を表示
     else:
         elem.clear()
+        # メモリを開放する
