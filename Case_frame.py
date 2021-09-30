a09b83ef4f90fd4660c46748c271557562c14251
yoshiwo115 <yoshiwo115@gmail.com>
2021-09-30 11:23:27 +0900

    関数化しました

--- Case_frame.py
+++ Case_frame.py
@@ -9,28 +9,33 @@ declinable_word = "綺麗"
 
 import xml.etree.ElementTree as ET
 
-# メモリ開放しながらxmlを読む
-
-context = ET.iterparse('kyoto-univ-web-cf-2.0.xml', events=('start', 'end'))
-
-for event,elem in context:
-    if event == 'start' and elem.tag == 'entry' and 'あえない' in elem.attrib['headword']: 
-        # headwordが特定の用言だったとき
-            
-            print(elem.attrib) #何で検索されているか表示
-            
-            for event, elem in context: 
-                #entry以下でループを回す
+def search_noun():
+    context = ET.iterparse('kyoto-univ-web-cf-2.0.xml', events=('start', 'end'))
+    # メモリ開放しながらxmlを読む
+    for event,elem in context:
+        if event == 'start' and elem.tag == 'entry' and declinable_word in elem.attrib['headword']: 
+            # headwordが特定の用言だったとき
                 
-                if event == 'end' and elem.tag == 'entry': 
-                    #entryのendタグが来たら
+                print(elem.attrib) #何で検索されているか表示
+                
+                for event, elem in context: 
+                    #entry以下でループを回す
                     
-                    break #ループを抜ける
+                    if event == 'end' and elem.tag == 'entry': 
+                        #entryのendタグが来たら 
+                        
+                        elem.clear()
+                        return 0 #ループを抜ける
 
-                elif event == 'end' and elem.tag == 'component': 
-                    #componentのendタグか来たら
-                    
-                    print(elem.text) #内容を表示
-    else:
-        elem.clear()
-        # メモリを開放する
+                    elif event == 'end' and elem.tag == 'component': 
+                        #componentのendタグか来たら
+                        
+                        print(elem.text) #内容を表示
+        else:
+            elem.clear()
+            # メモリを開放する
+
+
+search_noun()
+
+print('\n終わり')
